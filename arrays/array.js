d74d70f430f84d9ee33e0fe532e6506296489312
Harshinideva <you@example.com>
2024-09-20 18:12:04 +0530

    Initial commit

---
+++ arrays/array.js
@@ -0,0 +1,17 @@
+const myArray = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    function sum() {
+      const  a = 10
+      const b = 5
+      const c = a+b
+      return c
+
+    },
+    { name: 'harshu', company: 'securra' }
+];
+const a = myArray[0];
+const b = myArray[1 ]();
+const c = myArray[2];
+
+
+console.log(a,b,c); 
